d8258e4d4e6bdce76abc368e5370c2beb61ca735
barchiii <145505905+barchiii@users.noreply.github.com>
2024-01-09 11:08:01 +0100

    02_sconto.c
    Exercise 2 commented

--- 02_sconto.c
+++ 02_sconto.c
@@ -3,21 +3,30 @@
         da 0 a 500 sconto del 10%;
         da 501 a 1000 sconto del 20%;
         oltre i 1001 sconto 30%;
- Il programma dopo aver calcolato e visualizzato a video lo sconto ed il prezzo netto deve permettere di re-inserire
- ulteriori somme di denaro. */
+ Il programma dopo aver calcolato e visualizzato a video lo sconto ed il prezzo netto 
+ deve permettere di re-inserire ulteriori somme di denaro. */
+
+/* On a sum of money (related to the purchase of a product), 
+a discount is desired based on the entered amount following the following scheme:
+	- From 0 to 500, a discount of 10%;
+	- From 501 to 1000, a discount of 20%;
+	- Above 1001, a discount of 30%.
+The program, after calculating and displaying the discount and net price on the screen, 
+should allow for the re-insertion of additional sums of money. */
 
 #include <stdio.h>
 
-void main() {
+void main() {	
+    // Variable declaration
     float importo, sconto, importoSottratto, importoFinale;
     char continua;
 
     do {
-        // Input dell'importo
+        // Amount input
         printf("Inserisci l'importo dell'articolo: ");
         scanf("%f", &importo);
 
-        // Calcolo dello sconto in base all'importo
+        // Discount calculation based on the amount
         if (importo <= 500) {
             sconto = 0.10;
         } 
@@ -28,16 +37,16 @@ void main() {
             sconto = 0.30;
         }
 
-        // Calcolo dell'importo scontato e del prezzo netto
+        // Calculation of discounted amount and net price
         importoSottratto = importo * sconto;
         importoFinale = importoFinale + importo - importoSottratto;
 
-        // Visualizzazione dei risultati
+        // Output
         printf("Sconto applicato: %.2f%%\n", sconto * 100);
         printf("Importo sottratto: %.2f\n", importoSottratto);
         printf("Importo parziale: %.2f\n", importoFinale);
 
-        // Chiedi se si vuole inserire un'altra somma di denaro
+        // Ask if you want to enter another sum of money
         printf("Vuoi inserire un'altra somma di denaro? (s/n): ");
         scanf(" %c", &continua);
 
